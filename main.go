87d33059baadfaf9301dc2e2d03e38adfc7f2afb
Hexxenwerk <12483157+Hexxenwerk@users.noreply.github.com>
2020-12-07 12:30:30 +0100

    Update main.go
    Further simplification

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -11,13 +10,7 @@ func main() {
 	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
 		if _, err := fmt.Fprintln(rw, "Hello World"); err != nil {
 			log.Fatal(err)
-		d, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
-		if _, err := fmt.Fprintf(rw, "Data: %s\n", d); err != nil {
-			log.Fatal(err)
 		}
 	})
-	log.Fatalln(http.ListenAndServe("127.0.0.1:9090", nil))
+	log.Fatalln(http.ListenAndServe(":9090", nil))
 }
